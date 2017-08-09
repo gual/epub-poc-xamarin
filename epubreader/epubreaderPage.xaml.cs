9a81603d6692973e63f5006db68c2f8570c5e23e
Gual Escalante <gual23@gmail.com>
2017-08-08 23:03:57 -0600

    Initial commit

---
+++ epubreader/epubreaderPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using VersFx.Formats.Text.Epub;
+using Xamarin.Forms;
+
+namespace epubreader
+{
+	public partial class epubreaderPage : ContentPage
+	{
+		public epubreaderPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			var assembly = typeof(epubreaderPage).GetTypeInfo().Assembly;
+			Stream stream = assembly.GetManifestResourceStream("epubreader.Resources.conde.epub");
+			EpubBook epubBook = EpubReader.ReadBook(stream);
+			this.titleTxt.Text = epubBook.Title;
+            this.authorTxt.Text = epubBook.Author;
+
+			var chaptersList = "";
+			foreach (EpubChapter chapter in epubBook.Chapters)
+			{
+				// Title of chapter
+				chaptersList += chapter.Title + " ";
+			}
+
+			this.chapterList.Text = chaptersList;
+
+
+		}
+	}
+}
